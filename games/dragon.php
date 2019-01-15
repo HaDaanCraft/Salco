0f42e1686f601289bc1f2de016e7d626af5219e4
HaDaanCraft <hermans.daan@hotmail.com>
2019-01-15 20:24:41 +0100

    Started Dragon game

--- games/dragon.php
+++ games/dragon.php
@@ -1,8 +1,18 @@
-let springHoogte = 10
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Salco - 4B</title>
+    <link rel="icon" href="./assets/pictures/salcosecond.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/dragon.js"></script>
+  </head>
+  <body>
 
-Lopen(){
-  if spatie=pressed{
-      
-    }
-}
-
+    <div class="nav" id="nav"></div>
+    
+  </body>
+</html>
